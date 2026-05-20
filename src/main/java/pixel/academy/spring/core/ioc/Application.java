6a9307adeab8a6c371f01dbd1cefe4dd6e72b6ff
Cirlig Petru <cirligpetru96@gmail.com>
2026-05-20 18:22:18 +0200

    Initial commit

---
+++ src/main/java/pixel/academy/spring/core/ioc/Application.java
@@ -0,0 +1,13 @@
+package pixel.academy.spring.core.ioc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/pixel/academy/spring/core/ioc/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pixel.academy.spring.core.ioc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
